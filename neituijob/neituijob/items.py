a211bacba648ddda923f7c97efcc934ae725f06e
kzrs55 <569726429@qq.com>
2016-04-17 14:40:32 +0800

    定义爬取的items

--- neituijob/neituijob/items.py
+++ neituijob/neituijob/items.py
@@ -11,4 +11,11 @@ import scrapy
 class NeituijobItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
+    name = scrapy.Field()  # 职位名称
+    company = scrapy.Field()  # 公司名称
+    work_location = scrapy.Field()  # 工作地点
+    recruit_number = scrapy.Field()  # 招聘人数
+    detail_link = scrapy.Field()  # 职位详情页链接
+    publish_time = scrapy.Field()  # 发布时间
+    data = scrapy.Field()  # 职位详情
     pass
